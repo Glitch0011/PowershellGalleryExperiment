861e1eec76d84a73d58201712595f09f80176f78
Tom Bonner <tom@bonner.is>
2018-05-03 11:30:51 +0100

    Added logging

--- Generate-Module.ps1
+++ Generate-Module.ps1
@@ -1,3 +1,11 @@
+if (Test-Path ".\deploy") {
+    Remove-Item ".\deploy" -Force -Recurse
+}
+
+if (Test-Path ".\src\*.psd1") {
+    Remove-Item ".\src\*.psd1" -Force
+}
+
 # Properties
 $moduleName = "Write-Day"
 $description = "Writes the day to output"
@@ -11,32 +19,32 @@ $license = "https://github.com/Glitch0011/PowershellGalleryExperiment/blob/maste
 $version = "$(Get-Date -Format yy.MM.dd).$($Env:APPVEYOR_BUILD_NUMBER)";
 $file = Get-Item -Path ".\src\$($moduleName).*" | Where-Object { $_.Name -match ".*\.(psm1|ps1)" }
 
+Write-Output "Generating manifest"
 New-ModuleManifest -Path ".\src\$($moduleName).psd1" -Description $description -Author "Tom Bonner" -CompanyName $company -ModuleVersion $version -RootModule $file.Name -FunctionsToExport $functionsToExport -ProjectUri $project -LicenseUri $license -Tags $tags
-
-if (Test-Path ".\deploy") {
-    Remove-Item ".\deploy" -Force -Recurse
-}
 
 $tempNugetRepo = New-Item -ItemType Directory ".\nuget-feed\nuget\v2"
 $deployTarget = New-Item -ItemType Directory ".\deploy"
 
 try
 {
+    Write-Output "Bootstrapping NuGet"
     Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
     
+    Write-Output "Registering temp repository"    
     Register-PSRepository -Name "temp" -SourceLocation $tempNugetRepo.FullName
 
     $moduleName = (Get-Item "src").GetFiles("*.psd1").FullName
 
+    Write-Output "Publishing module to temp repository"
     Publish-Module -Name $moduleName -Repository "temp"
 
     $package = Get-ChildItem -Filter "*.nupkg" -Recurse
 
+    Write-Output "Moving package to output"
     Move-Item -Path $package.FullName -Destination $deployTarget.FullName
 }
 finally 
 {
     Unregister-PSRepository "temp" -ErrorAction SilentlyContinue
     Remove-Item -Path (Get-Item "nuget-feed") -Recurse -Force
-    Remove-Item -Path (Get-Item ".\src\Write-Day.psd1")
 }
